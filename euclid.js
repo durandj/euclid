ebdd3485ba3e70d6d33124ae50500601e8f4fc39
James Durand <james.durand@alumni.msoe.edu>
2014-06-26 08:33:32 -0500

    Added line drawing

--- euclid.js
+++ euclid.js
@@ -3,7 +3,25 @@ function Point(x, y) {
 	this.y = y;
 }
 
-function drawPoint(canvas, point) {
+function Shape(type, points) {
+	this.type = type;
+
+	if (points instanceof Array) {
+		this.points = points;
+	}
+	else {
+		this.points = [points];
+	}
+}
+
+Shape.prototype.draw = function (canvas) {
+	this.points.unshift(canvas);
+	drawFunctions[this.type].apply(undefined, this.points);
+	this.points.shift(canvas);
+};
+
+var drawFunctions = {};
+drawFunctions.point = function (canvas, point) {
 	canvas.drawEllipse(
 		{
 			fillStyle: '#000',
@@ -13,4 +31,17 @@ function drawPoint(canvas, point) {
 			height: 5,
 		}
 	);
-}
+};
+
+drawFunctions.line_point_to_point = function (canvas, point0, point1) {
+	canvas.drawLine(
+		{
+			strokeStyle: '#000',
+			strokeWidth: 1,
+			x1: point0.x,
+			y1: point0.y,
+			x2: point1.x,
+			y2: point1.y,
+		}
+	);
+};
